b2bc56b4b33b540190b4c36ab7eb7ae5694e4320
User <User@DESKTOP-QHG18FL>
2025-10-01 16:42:08 +0300

    DisciplinesDirectionsRelation in propgress

--- ADO_NET/DataSet/MainForm.Designer.cs
+++ ADO_NET/DataSet/MainForm.Designer.cs
@@ -30,13 +30,25 @@
 		{
 			this.comboBoxStudentsGroup = new System.Windows.Forms.ComboBox();
 			this.comboBoxStudentsDirection = new System.Windows.Forms.ComboBox();
+			this.tabControl = new System.Windows.Forms.TabControl();
+			this.tabPage1 = new System.Windows.Forms.TabPage();
+			this.tabPage2 = new System.Windows.Forms.TabPage();
+			this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+			this.tabPage3 = new System.Windows.Forms.TabPage();
+			this.tabPageDisciplines = new System.Windows.Forms.TabPage();
+			this.comboBoxDisciplinesForDirection = new System.Windows.Forms.ComboBox();
+			this.dataGridViewDisciplines = new System.Windows.Forms.DataGridView();
+			this.tabControl.SuspendLayout();
+			this.tabPage1.SuspendLayout();
+			this.tabPageDisciplines.SuspendLayout();
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewDisciplines)).BeginInit();
 			this.SuspendLayout();
 			// 
 			// comboBoxStudentsGroup
 			// 
 			this.comboBoxStudentsGroup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.comboBoxStudentsGroup.FormattingEnabled = true;
-			this.comboBoxStudentsGroup.Location = new System.Drawing.Point(12, 12);
+			this.comboBoxStudentsGroup.Location = new System.Drawing.Point(6, 6);
 			this.comboBoxStudentsGroup.Name = "comboBoxStudentsGroup";
 			this.comboBoxStudentsGroup.Size = new System.Drawing.Size(249, 21);
 			this.comboBoxStudentsGroup.TabIndex = 0;
@@ -45,22 +57,112 @@
 			// 
 			this.comboBoxStudentsDirection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
 			this.comboBoxStudentsDirection.FormattingEnabled = true;
-			this.comboBoxStudentsDirection.Location = new System.Drawing.Point(267, 12);
+			this.comboBoxStudentsDirection.Location = new System.Drawing.Point(261, 6);
 			this.comboBoxStudentsDirection.Name = "comboBoxStudentsDirection";
 			this.comboBoxStudentsDirection.Size = new System.Drawing.Size(247, 21);
 			this.comboBoxStudentsDirection.TabIndex = 1;
 			this.comboBoxStudentsDirection.SelectedIndexChanged += new System.EventHandler(this.comboBoxStudentsDirection_SelectedIndexChanged);
+			// 
+			// tabControl
+			// 
+			this.tabControl.Controls.Add(this.tabPage1);
+			this.tabControl.Controls.Add(this.tabPage2);
+			this.tabControl.Controls.Add(this.tabPage3);
+			this.tabControl.Controls.Add(this.tabPageDisciplines);
+			this.tabControl.Location = new System.Drawing.Point(13, 12);
+			this.tabControl.Name = "tabControl";
+			this.tabControl.SelectedIndex = 0;
+			this.tabControl.Size = new System.Drawing.Size(775, 390);
+			this.tabControl.TabIndex = 2;
+			// 
+			// tabPage1
+			// 
+			this.tabPage1.Controls.Add(this.comboBoxStudentsGroup);
+			this.tabPage1.Controls.Add(this.comboBoxStudentsDirection);
+			this.tabPage1.Location = new System.Drawing.Point(4, 22);
+			this.tabPage1.Name = "tabPage1";
+			this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage1.Size = new System.Drawing.Size(767, 364);
+			this.tabPage1.TabIndex = 0;
+			this.tabPage1.Text = "tabPage1";
+			this.tabPage1.UseVisualStyleBackColor = true;
+			// 
+			// tabPage2
+			// 
+			this.tabPage2.Location = new System.Drawing.Point(4, 22);
+			this.tabPage2.Name = "tabPage2";
+			this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage2.Size = new System.Drawing.Size(192, 74);
+			this.tabPage2.TabIndex = 1;
+			this.tabPage2.Text = "tabPage2";
+			this.tabPage2.UseVisualStyleBackColor = true;
+			// 
+			// statusStrip1
+			// 
+			this.statusStrip1.Location = new System.Drawing.Point(0, 428);
+			this.statusStrip1.Name = "statusStrip1";
+			this.statusStrip1.Size = new System.Drawing.Size(800, 22);
+			this.statusStrip1.TabIndex = 3;
+			this.statusStrip1.Text = "statusStrip1";
+			// 
+			// tabPage3
+			// 
+			this.tabPage3.Location = new System.Drawing.Point(4, 22);
+			this.tabPage3.Name = "tabPage3";
+			this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPage3.Size = new System.Drawing.Size(767, 364);
+			this.tabPage3.TabIndex = 2;
+			this.tabPage3.Text = "tabPage3";
+			this.tabPage3.UseVisualStyleBackColor = true;
+			// 
+			// tabPageDisciplines
+			// 
+			this.tabPageDisciplines.Controls.Add(this.dataGridViewDisciplines);
+			this.tabPageDisciplines.Controls.Add(this.comboBoxDisciplinesForDirection);
+			this.tabPageDisciplines.Location = new System.Drawing.Point(4, 22);
+			this.tabPageDisciplines.Name = "tabPageDisciplines";
+			this.tabPageDisciplines.Padding = new System.Windows.Forms.Padding(3);
+			this.tabPageDisciplines.Size = new System.Drawing.Size(767, 364);
+			this.tabPageDisciplines.TabIndex = 3;
+			this.tabPageDisciplines.Text = "Disciplines";
+			this.tabPageDisciplines.UseVisualStyleBackColor = true;
+			// 
+			// comboBoxDisciplinesForDirection
+			// 
+			this.comboBoxDisciplinesForDirection.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+			this.comboBoxDisciplinesForDirection.FormattingEnabled = true;
+			this.comboBoxDisciplinesForDirection.Location = new System.Drawing.Point(210, 7);
+			this.comboBoxDisciplinesForDirection.Name = "comboBoxDisciplinesForDirection";
+			this.comboBoxDisciplinesForDirection.Size = new System.Drawing.Size(315, 21);
+			this.comboBoxDisciplinesForDirection.TabIndex = 0;
+			this.comboBoxDisciplinesForDirection.SelectedIndexChanged += new System.EventHandler(this.comboBoxDisciplinesForDirection_SelectedIndexChanged);
+			// 
+			// dataGridViewDisciplines
+			// 
+			this.dataGridViewDisciplines.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.dataGridViewDisciplines.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+			this.dataGridViewDisciplines.Location = new System.Drawing.Point(7, 35);
+			this.dataGridViewDisciplines.Name = "dataGridViewDisciplines";
+			this.dataGridViewDisciplines.Size = new System.Drawing.Size(757, 323);
+			this.dataGridViewDisciplines.TabIndex = 1;
 			// 
 			// MainForm
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(800, 450);
-			this.Controls.Add(this.comboBoxStudentsDirection);
-			this.Controls.Add(this.comboBoxStudentsGroup);
+			this.Controls.Add(this.statusStrip1);
+			this.Controls.Add(this.tabControl);
 			this.Name = "MainForm";
 			this.Text = "Form1";
+			this.tabControl.ResumeLayout(false);
+			this.tabPage1.ResumeLayout(false);
+			this.tabPageDisciplines.ResumeLayout(false);
+			((System.ComponentModel.ISupportInitialize)(this.dataGridViewDisciplines)).EndInit();
 			this.ResumeLayout(false);
+			this.PerformLayout();
 
 		}
 
@@ -68,6 +170,14 @@
 
 		private System.Windows.Forms.ComboBox comboBoxStudentsGroup;
 		private System.Windows.Forms.ComboBox comboBoxStudentsDirection;
+		private System.Windows.Forms.TabControl tabControl;
+		private System.Windows.Forms.TabPage tabPage1;
+		private System.Windows.Forms.TabPage tabPage2;
+		private System.Windows.Forms.StatusStrip statusStrip1;
+		private System.Windows.Forms.TabPage tabPage3;
+		private System.Windows.Forms.TabPage tabPageDisciplines;
+		private System.Windows.Forms.DataGridView dataGridViewDisciplines;
+		private System.Windows.Forms.ComboBox comboBoxDisciplinesForDirection;
 	}
 }
 
--- ADO_NET/DataSet/MainForm.cs
+++ ADO_NET/DataSet/MainForm.cs
@@ -19,12 +19,18 @@ namespace DataSet
 		string connectionString = "";
 		SqlConnection connection = null;
 		System.Data.DataSet GroupsRelatedData = null;
+		System.Data.DataSet DisciplinesDirectionsRelation = null;
 		public MainForm()
 		{
 			InitializeComponent();
 			connectionString = ConfigurationManager.ConnectionStrings["PD_321"].ConnectionString;
 			connection = new SqlConnection(connectionString);
 
+			InitGroupsRelatedData();
+			InitDisciplinesDirectionsRelation();
+		}
+		void InitGroupsRelatedData()
+		{
 			//1) Создаем DataSet:
 			GroupsRelatedData = new System.Data.DataSet(nameof(GroupsRelatedData));
 
@@ -110,6 +116,83 @@ namespace DataSet
 			comboBoxStudentsGroup.SelectedIndexChanged += new EventHandler(GetKeyValue);
 			comboBoxStudentsDirection.SelectedIndexChanged += new EventHandler(GetKeyValue);
 
+		}
+		void InitDisciplinesDirectionsRelation()
+		{
+			DisciplinesDirectionsRelation = new System.Data.DataSet(nameof(DisciplinesDirectionsRelation));
+
+			string dsTable_Disciplines = "Disciplines";
+			string dstDisciplines_discipline_id = "discipline_id";
+			string dstDisciplines_discipline_name = "discipline_name";
+			string dstDisciplines_number_of_lessons = "number_of_lessons";
+			DisciplinesDirectionsRelation.Tables.Add(dsTable_Disciplines);
+			DisciplinesDirectionsRelation.Tables[dsTable_Disciplines].Columns.Add(dstDisciplines_discipline_id);
+			DisciplinesDirectionsRelation.Tables[dsTable_Disciplines].Columns.Add(dstDisciplines_discipline_name);
+			DisciplinesDirectionsRelation.Tables[dsTable_Disciplines].Columns.Add(dstDisciplines_number_of_lessons);
+			DisciplinesDirectionsRelation.Tables[dsTable_Disciplines].PrimaryKey =
+				new DataColumn[] { DisciplinesDirectionsRelation.Tables[dsTable_Disciplines].Columns[dstDisciplines_discipline_id] };
+
+			string dsTable_Directions = "Directions";
+			string dstDirections_direction_id = "direction_id";
+			string dstDirections_direction_name = "direction_name";
+			DisciplinesDirectionsRelation.Tables.Add(dsTable_Directions);
+			DisciplinesDirectionsRelation.Tables[dsTable_Directions].Columns.Add(dstDirections_direction_id);
+			DisciplinesDirectionsRelation.Tables[dsTable_Directions].Columns.Add(dstDirections_direction_name);
+			DisciplinesDirectionsRelation.Tables[dsTable_Directions].PrimaryKey =
+				new DataColumn[] { DisciplinesDirectionsRelation.Tables[dsTable_Directions].Columns[dstDirections_direction_id] };
+
+			string dsTable_DDR = "DisciplinesDirectionsRelation";
+			string dstDDR_direction = "direction";
+			string dstDDR_discipline = "discipline";
+			DisciplinesDirectionsRelation.Tables.Add(dsTable_DDR);
+			DisciplinesDirectionsRelation.Tables[dsTable_DDR].Columns.Add(dstDDR_direction);
+			DisciplinesDirectionsRelation.Tables[dsTable_DDR].Columns.Add(dstDDR_discipline);
+			DisciplinesDirectionsRelation.Tables[dsTable_DDR].PrimaryKey =
+				new DataColumn[]
+				{
+					DisciplinesDirectionsRelation.Tables[dsTable_DDR].Columns[dstDDR_discipline],
+					DisciplinesDirectionsRelation.Tables[dsTable_DDR].Columns[dstDDR_direction]
+				};
+
+			string dsrDiscipline = "Discipline";
+			string dsrDirection = "Direction";
+			DisciplinesDirectionsRelation.Relations.Add
+				(
+				new DataRelation
+					(
+						dsrDiscipline,
+						DisciplinesDirectionsRelation.Tables[dsTable_Disciplines].Columns[dstDisciplines_discipline_id],
+						DisciplinesDirectionsRelation.Tables[dsTable_DDR].Columns[dstDDR_discipline]
+					)
+				);
+			DisciplinesDirectionsRelation.Relations.Add
+				(
+					new DataRelation
+					(
+						dsrDirection,
+						DisciplinesDirectionsRelation.Tables[dsTable_Directions].Columns[dstDirections_direction_id],
+						DisciplinesDirectionsRelation.Tables[dsTable_DDR].Columns[dstDDR_direction]
+					)
+				);
+
+			string cmd_disciplines = "SELECT * FROM Disciplines";
+			string cmd_directions = "SELECT * FROM Directions";
+			string cmd_DDR = $"SELECT * FROM {dsTable_DDR}";
+
+			SqlDataAdapter disciplinesAdapter = new SqlDataAdapter(cmd_disciplines, connection);
+			SqlDataAdapter directionAdapter = new SqlDataAdapter(cmd_directions, connection);
+			SqlDataAdapter DDRAdapter = new SqlDataAdapter(cmd_DDR, connection);
+
+			disciplinesAdapter.Fill(DisciplinesDirectionsRelation.Tables[dsTable_Disciplines]);
+			directionAdapter.Fill(DisciplinesDirectionsRelation.Tables[dsTable_Directions]);
+			DDRAdapter.Fill(DisciplinesDirectionsRelation.Tables[dsTable_DDR]);
+
+			///////////////////////////////////////////////////////////
+
+			dataGridViewDisciplines.DataSource = DisciplinesDirectionsRelation.Tables[dsTable_Disciplines];
+			comboBoxDisciplinesForDirection.DataSource = DisciplinesDirectionsRelation.Tables[dsTable_Directions];
+			comboBoxDisciplinesForDirection.DisplayMember = DisciplinesDirectionsRelation.Tables[dsTable_Directions].Columns[dstDirections_direction_name].ToString();
+			comboBoxDisciplinesForDirection.ValueMember = DisciplinesDirectionsRelation.Tables[dsTable_Directions].Columns[dstDirections_direction_id].ToString();
 
 		}
 		void GetKeyValue(object sender, EventArgs e)
@@ -123,11 +206,28 @@ namespace DataSet
 
 		private void comboBoxStudentsDirection_SelectedIndexChanged(object sender, EventArgs e)
 		{
-			comboBoxStudentsGroup.DataSource = 
-				GroupsRelatedData.Tables["Groups"].Select
-				(
-					$"direction={comboBoxStudentsDirection.SelectedValue}"
-				).CopyToDataTable();
+			try
+			{
+				comboBoxStudentsGroup.Enabled = true;
+				comboBoxStudentsGroup.DataSource =
+						GroupsRelatedData.Tables["Groups"].Select
+						(
+							$"direction={comboBoxStudentsDirection.SelectedValue}"
+						).CopyToDataTable();
+			}
+			catch (Exception ex)
+			{
+				comboBoxStudentsGroup.Enabled = false;
+			}
+		}
+
+		private void comboBoxDisciplinesForDirection_SelectedIndexChanged(object sender, EventArgs e)
+		{
+			dataGridViewDisciplines.DataSource =
+				DisciplinesDirectionsRelation
+				.Tables["DisciplinesDirectionsRelation"]
+				.ParentRelations["Discipline"]
+				.ParentTable;
 		}
 	}
 }
