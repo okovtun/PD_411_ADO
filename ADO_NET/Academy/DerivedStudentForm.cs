d38121d9769c6a88e0f06dcc30f6152828f88a1d
User <User@DESKTOP-QHG18FL>
2025-10-10 18:46:05 +0300

    Some improvements

--- ADO_NET/Academy/DerivedStudentForm.cs
+++ ADO_NET/Academy/DerivedStudentForm.cs
@@ -28,7 +28,7 @@ namespace Academy
 		protected override void Extract()
 		{
 			base.Extract();
-			comboBoxGroup.SelectedIndex = (Human as Student).Group;
+			comboBoxGroup.SelectedValue = (Human as Student).Group;
 			labelID.Text = (Human as Student).ID.ToString();
 		}
 		protected override void buttonOK_Click(object sender, EventArgs e)
