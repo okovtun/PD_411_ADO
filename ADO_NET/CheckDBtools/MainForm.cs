52da98005fe625fe49e2473daa81f51dedbfabfa
User <User@DESKTOP-QHG18FL>
2025-10-17 12:58:52 +0300

    DBtools DLL DONE

---
+++ ADO_NET/CheckDBtools/MainForm.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Runtime.InteropServices;
+
+using DBtools;
+
+namespace CheckDBtools
+{
+	public partial class MainForm : Form
+	{
+		public MainForm()
+		{
+			InitializeComponent();
+			AllocConsole();
+			Connector connector = new Connector();
+			dataGridView.DataSource = connector.Select("*", "Teachers");
+		}
+		[DllImport("kernel32.dll")]
+		public static extern bool AllocConsole();
+	}
+}
